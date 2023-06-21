2c123e658712789a35368a330d720c153948c0f8
Madison Bullard <madison.bullard@gmail.com>
2023-06-20 21:11:30 -0400

    cache

--- api/sanityDocuments/getSanityDocument.ts
+++ api/sanityDocuments/getSanityDocument.ts
@@ -3,13 +3,11 @@ import 'server-only'
 import { cache } from 'react'
 import { SanityDocument } from 'sanity'
 
-import { client } from '../../sanity/lib/client'
+import { clientFetch } from '../../sanity/lib/client'
 
-const getSanityDocument = cache(
-  async <T extends SanityDocument>(id: string) => {
-    const query = `*[_id == "${id}"][0]`
-    return await client.fetch<T | undefined>(query)
-  }
-)
+const getSanityDocument = async <T extends SanityDocument>(id: string) => {
+  const query = `*[_id == "${id}"][0]`
+  return await clientFetch<T | undefined>(query)
+}
 
 export default getSanityDocument
