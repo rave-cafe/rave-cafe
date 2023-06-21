8581a05e184ceff407ee449d84eccf18d4ef7628
Madison Bullard <madison.bullard@gmail.com>
2023-06-20 21:07:16 -0400

    Added NavBar + update Tailwind config (#14)
    * k
    
    * remove viewport stuff for now
    
    * svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
-import 'styles/global.css'
+import 'styles/global.css';
+
+import NavBar from 'components/NavBar';
 
 export default function RootLayout({
   children,
@@ -7,7 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
