85bd9d2505c52ab687b7602a90f87223b0a7bf36
Madison Bullard <madison.bullard@gmail.com>
2023-06-22 19:08:46 -0400

    merge

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import getPosts from 'api/post/getPosts'
+import Post from 'components/Post'
+import PATHS from 'constants/paths'
+
+export default async function Page() {
+  const posts = await getPosts()
+
+  return posts.map((post) => (
+    <Post key={post._id} href={`/${PATHS.BLOG}/${post.slug}`} {...post} />
+  ))
+}
