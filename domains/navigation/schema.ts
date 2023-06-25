ca2c80da3af13d70f24fd4f79a80f547da4db4da
Madison Bullard <madison.bullard@gmail.com>
2023-06-25 19:00:03 -0400

    navigation

---
+++ domains/navigation/schema.ts
@@ -0,0 +1,45 @@
+import { SchemaTypeDefinition } from 'sanity'
+
+const navigationSchema: SchemaTypeDefinition = {
+  name: 'person',
+  title: 'Person',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        //Change to schema title to automatically populate
+        source: 'name',
+        slugify: (input) =>
+          input
+            .toLowerCase()
+            //Remove spaces
+            .replace(/\s+/g, '-')
+            //Remove special characters
+            .replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, ''),
+        validation: (Rule) => Rule.required(),
+      },
+    },
+    {
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+    },
+    {
+      name: 'bio',
+      of: [{ type: 'block' }],
+      title: 'Bio',
+      type: 'array',
+    },
+  ],
+}
+
+export default navigationSchema
