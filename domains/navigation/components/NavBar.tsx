e13c31cdeb34ded33c1f6dd5deba8a488954ca1d
Madison Bullard <madison.bullard@gmail.com>
2023-06-25 19:22:41 -0400

    sticky

--- domains/navigation/components/NavBar.tsx
+++ domains/navigation/components/NavBar.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 export default function NavBar() {
   return (
-    <div className="absolute bg-gray-400">
-      <Link className="relative m-1 flex" href="/">
-        <Image alt="Rave Cafe logo" height={40} width={40} src="/rc-logo.svg" />
+    <div className="fixed m-1 rounded-full bg-yellow-300">
+      <Link className="relative flex" href="/">
+        <Image alt="Rave Cafe logo" height={48} width={48} src="/rc-logo.svg" />
       </Link>
     </div>
   )
